40eb953c78593e5baafb1bcd8679e3e4bc0517c3
John Dillinger <higz-06@hotmail.com>
2018-10-28 11:39:51 +0000

    added link to room-availability check page, increased animation delays

--- main.js
+++ main.js
@@ -3,26 +3,26 @@ const intToDay = i => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'][i
 (async () => {
   try {
     const response = await fetch('https://itsligo-utils.herokuapp.com/timetable/Sg_KSDEV_B07-F-Y2-1-(A)');
-    const res = await response.json();
+    const json = await response.json();
     const group = document.getElementById('group');
 
-    for (let i = 0; i < res.data.length; i += 1) { // Create headers and badges
+    for (let i = 0; i < json.data.length; i += 1) { // Create headers and badges
       const header = document.createElement('a');
       const isToday = new Date().getDay() - 1 === i;
       header.innerHTML = intToDay(i);
       header.className = 'list-group-item list-group-item-action mt-1 font-weight-bold animated fadeIn';
       if (isToday) header.classList += ' text-danger';
       const badge = document.createElement('span');
-      badge.innerHTML = res.data[i].length;
+      badge.innerHTML = json.data[i].length;
       badge.className = isToday
         ? 'badge badge-info float-right animated fadeIn badge-danger'
         : 'badge badge-info float-right animated fadeIn badge-dark';
       header.append(badge);
       group.appendChild(header);
 
-      for (let j = 0; j < res.data[i].length; j += 1) { // Create classes
+      for (let j = 0; j < json.data[i].length; j += 1) { // Create classes
         const a = document.createElement('a');
-        const currClass = res.data[i][j];
+        const currClass = json.data[i][j];
         const splitClassName = currClass.name.split('/');
         if (splitClassName[0].includes(' GD & SD')) splitClassName[0] = splitClassName[0].replace(/ GD & SD/, '');
         const splitRoom = currClass.room.split('(');
