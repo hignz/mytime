9ebf15631b00feaa6e56044c36f6fbed16cf0788
hignz <higz-06@hotmail.com>
2019-01-10 23:23:15 +0000

    ...

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ async function fillDropDown(callback) {
         opt.text = json[i].title || json[i].course;
         opt.value = json[i].course;
         const select = document.getElementById('courseData');
-        const sel = document.getElementById('courseSel');
         select.append(opt);
       }
       if (callback) callback();
