3a760f3894a2486175b95b582240479b38c8d7e0
hignz <higz-06@hotmail.com>
2019-01-18 16:23:52 +0000

    es6 imports, eslint support, minified css

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "env": {
+    "browser": true,
+  },
+  'extends': 'airbnb-base',
+  'rules': {
+    'no-console': 'off',
+    'no-nested-ternary': 'off',
+    "no-console": "off",
+    "import/order": "off",
+    "space-before-function-paren": ["error", "always"],
+    "no-restricted-syntax": "off",
+    "no-continue": "off",
+    "no-param-reassign": "off"
+  }
+};
