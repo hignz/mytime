76672c3610c574b69b57ecf933978d59d2102ab9
hignz <higz-06@hotmail.com>
2019-09-12 04:00:11 +0100

    Deploy to gh-pages

---
+++ precache-manifest.75bf3195254240feb9d037386be93d10.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1790a47a0a01cca75784f4da0f91a28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "682f4d4657be9a63371e",
+    "url": "main.css"
+  },
+  {
+    "revision": "682f4d4657be9a63371e",
+    "url": "main.js"
+  },
+  {
+    "revision": "877af333d82cff677fb198d32d85eae0",
+    "url": "main.js.LICENSE"
+  }
+]);
