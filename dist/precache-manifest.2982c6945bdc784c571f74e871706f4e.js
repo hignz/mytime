bb395da436c71c56c92f01851595f8ff46f2e92a
hignz <higz-14@live.com>
2019-12-04 04:18:50 +0000

    Deploy to gh-pages

---
+++ dist/precache-manifest.2982c6945bdc784c571f74e871706f4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2f5927dc9b2cfecd294736387b3705",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4756455308f5a3c616d6",
+    "url": "main.css"
+  },
+  {
+    "revision": "4756455308f5a3c616d6",
+    "url": "main.js"
+  },
+  {
+    "revision": "c17dab0f0f70e2802b814ba65a7f8a43",
+    "url": "main.js.LICENSE"
+  }
+]);
